952d2322a1e3628329f0a99b0213be5048f1e41a
gupadhyaya <ganeshrvce@gmail.com>
2016-02-25 23:04:11 -0600

    sampletest

---
+++ src/test/java/org/paninij/analyses/SampleTest.java
@@ -0,0 +1,46 @@
+package org.paninij.analyses;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import static org.paninij.analyses.TestProcessorRunner.processJavaSourceResourceFiles;
+import static java.util.Arrays.asList;
+
+@RunWith(Parameterized.class)
+public class SampleTest {
+	private final static String SRC_DIR_PREFIX = "org/paninij/proc/helloworld/";
+	private final String resourceFile;
+	
+	public SampleTest(String resourceFile) {
+		this.resourceFile = resourceFile;
+	}
+
+	@Parameters(name = "{0}")
+	public static Collection<String> knownTestResource() {
+		List<String> resources = asList(
+		"ConsoleTemplate.java",
+		"GreeterTemplate.java",
+		"HelloWorldShortTemplate.java",
+		"HelloWorldTemplate.java",
+		"StreamTemplate.java"
+		);
+		
+		// Add prefix to each resource.
+        for (int i = 0; i < resources.size(); i++) {
+            resources.set(i, SRC_DIR_PREFIX + resources.get(i));
+        }
+        
+        return resources;
+	}
+	
+	@Test
+    public void processResourceFile() {
+        processJavaSourceResourceFiles(resourceFile);
+    }
+
+}
